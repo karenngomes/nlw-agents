32ffad71d958d6d441202b8852f9da267e20ea95
Karen Gomes <karenngomes.dev@gmail.com>
2025-07-10 21:48:38 -0300

    feat(web): add react-query and show rooms

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,9 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
+type GetRoomsAPIResponse = Array<{
+  id: string;
+  name: string;
+}>;
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-rooms"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/rooms");
+      const result: GetRoomsAPIResponse = await response.json();
+      return result;
+    },
+  });
+
   return (
     <div>
-      <div>Create Room Page</div>
+      {isLoading && <p>Loading rooms...</p>}
+
+      {data?.map((room) => (
+        <div key={room.id}>
+          <p>{room.name}</p>
+        </div>
+      ))}   
 
       <Link className="underline" to="/room">
         Go to Room Page
