6c6385e3a7531e10c3e36639d305f75a32d33b08
Karen Gomes <karenngomes.dev@gmail.com>
2025-07-14 19:44:01 -0300

    feat(server): generate answer

--- server/src/services/gemini.ts
+++ server/src/services/gemini.ts
@@ -49,3 +49,40 @@ export async function generateEmbeddings(text: string) {
 
   return response.embeddings[0].values;
 }
+
+export async function generateAnswer(
+  question: string,
+  transcriptions: string[]
+) {
+  const context = transcriptions.join("\n\n");
+
+  const prompt = `
+        Com base no texto fornecido abaixo como contexto, responda à pergunta de forma clara e precisa em português do Brasil.
+
+        CONTEXTO: ${context}
+        PERGUNTA: ${question}
+
+        INSTRUÇÕES:
+        - Use apenas as informações contidas no contexto enviado;
+        - Se a resposta não for encontrada no contexto, apenas responda que não possui informações suficientes para responder;
+        - Seja objetivo;
+        - Mantenha um tom educativo e profissional;
+        - Cite trechos importantes do contexto se apropriado;
+        - Se for citar o contexto, utilize o termo "conteúdo da aula".
+    `.trim();
+
+  const response = await gemini.models.generateContent({
+    model,
+    contents: [
+      {
+        text: prompt,
+      },
+    ],
+  });
+
+  if (!response.text) {
+    throw new Error("Failed to generate answer.");
+  }
+
+  return response.text;
+}
