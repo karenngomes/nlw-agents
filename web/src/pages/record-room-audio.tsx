91daefa474b40c9204fa7bd73ac77d5e99c6c796
Karen Gomes <karenngomes.dev@gmail.com>
2025-07-14 17:05:58 -0300

    feat(web): create record room audio page

---
+++ web/src/pages/record-room-audio.tsx
@@ -0,0 +1,97 @@
+import { useRef, useState } from "react";
+import { Button } from "../components/ui/button";
+import { Navigate, useParams } from "react-router-dom";
+
+const isRecordingSupported =
+  !!navigator.mediaDevices &&
+  typeof navigator.mediaDevices.getUserMedia === "function" &&
+  typeof window.MediaRecorder === "function";
+
+type RoomParams = {
+  roomId: string;
+};
+
+export function RecordRoomAudio() {
+  const params = useParams<RoomParams>();
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
+
+  const [isRecording, setIsRecording] = useState(false);
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
+
+  function stopRecording() {
+    setIsRecording(false);
+
+    if (mediaRecorder.current && mediaRecorder.current.state !== "inactive") {
+      mediaRecorder.current.stop();
+    }
+  }
+
+  async function uploadAudio(audio: Blob) {
+    console.log("Uploading audio:", audio);
+    const formData = new FormData();
+
+    formData.append("file", audio, "audio.webm");
+
+    // const response = await fetch(
+    //   `http://localhost:3333/rooms/${params.roomId}/audio`,
+    //   {
+    //     method: "POST",
+    //     body: formData,
+    //   }
+    // );
+
+    // const result = await response.json();
+    // console.log("Upload result:", result);
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert("Gravação de áudio não é suportada neste navegador.");
+      return;
+    }
+    setIsRecording(true);
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    });
+
+    mediaRecorder.current = new MediaRecorder(audio, {
+      mimeType: "audio/webm",
+      audioBitsPerSecond: 64_000,
+    });
+
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        console.log("Audio data available:", event.data);
+        uploadAudio(event.data);
+      }
+    };
+
+    mediaRecorder.current.onstart = () => {
+      console.log("Recording started");
+    };
+    mediaRecorder.current.onstop = () => {
+      console.log("Recording stopped");
+    };
+
+    mediaRecorder.current.start();
+  }
+
+  return (
+    <div className="h-screen  flex items-center justify-center gap-3 flex-col">
+      {isRecording ? (
+        <Button onClick={stopRecording}>Parar Gravação</Button>
+      ) : (
+        <Button onClick={startRecording}>Iniciar Gravação</Button>
+      )}
+      {isRecording ? <p>Gravando...</p> : <p>Pausado</p>}
+    </div>
+  );
+}
